37f01e730554bcb82d69043104cc83dc093fad04
Иван Картавый <kartavey@gmail.com>
2022-10-02 20:19:37 +0300

    Исправил код для корректной работы. Добавил меню. Использовал while (true).

--- src/Practicum.java
+++ src/Practicum.java
@@ -4,22 +4,33 @@ public class Practicum {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите год:");
-        int year = scanner.nextInt();
 
-        if (isLeapYear(year)) {
-            System.out.println("12.09."+year);// здесь нужно вывести результат
-        } else {
-            System.out.println("13.09."+year);
+        while (true) {
+            System.out.println("1 - Ввести год.");
+            System.out.println("2 - Выход.");
+            int choose = scanner.nextInt();
+            if (choose == 1) {
+                System.out.println("Введите год:");
+                int year = scanner.nextInt();
+                if (isLeapYear(year)) {
+                    System.out.println("12.09." + year + " - Високостный год.");// здесь нужно вывести результат
+                } else {
+                    System.out.println("13.09." + year + " - Не високостный год.");
+                }
+            } else if (choose == 2) {
+                break;
+            }
         }
     }
-
     public static boolean isLeapYear(int year) {
         if (year % 4 != 0){
             return false;// здесь нужно определить, является ли переданный год високосным
-        }else if (year % 400 == 0) {
+        }else if (year % 400 != 0) {
+            return false;
+        } else if (year % 100 == 0){
+            return true;
+        } else{
             return true;
         }
-        return true;
     }
 }
